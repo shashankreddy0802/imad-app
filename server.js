dbc13cb72235ecffbb16b2d1bb56c13119c0df2e
shashankreddy0802 <30977583+shashankreddy0802@users.noreply.github.com>
2017-09-15 20:44:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config= {
+    user:'shashankreddy0802',
+    database:'shashankreddy0802',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:PROCESS.ENV.DB_PASSWORD
+    
+}
 
 var app = express();
 app.use(morgan('combined'));
+
+var Pool=new Pool(config)
+app.get('/test-db' ,function (req,res){
+pool.query('SELECT * FROM test',function(err,result){
+    if (err){
+        res.status(500).send(err.tostring());
+    }else{
+        res.send(JSON.stringify(result));
+    }
+ });
+    
+ });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -18,9 +39,6 @@ app.get('/article-three',function(req,res){
     res.send('article three is requested and it will be served');
 });
 
-app.get('/test-db' ,function (req,res){
-    
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
